673c50370ecb71dc302c9c8f4d101dfdcc6763ed
NairaGal <I24.galstyan.naira@etud.ufar.am>
2025-12-10 21:14:03 +0400

    Update README with project overview and usage instructions

--- include/vector.h
+++ include/vector.h
@@ -2,24 +2,30 @@
 #define VECTOR_H
 
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 
+// Generic vector structure
 typedef struct {
-    void *data;
-    size_t size;
-    size_t capacity;
-    size_t elem_size;
+    void *data;       // pointer to the data
+    size_t size;      // number of elements currently stored
+    size_t capacity;  // total allocated elements
+    size_t elem_size; // size of each element
 } Vector;
 
+// Create and destroy
 Vector* vector_create(size_t elem_size, size_t capacity);
-void vector_destroy(Vector *v);
-void vector_push_back(Vector *v, void *elem);
-void vector_pop_back(Vector *v);
-void* vector_get(Vector *v, size_t index);
-void vector_set(Vector *v, size_t index, void *elem);
-size_t vector_size(Vector *v);
-size_t vector_capacity(Vector *v);
+void vector_destroy(Vector* v);
+
+// Get size and capacity
+size_t vector_size(Vector* v);
+size_t vector_capacity(Vector* v);
+
+// Add/remove elements
+void vector_push_back(Vector* v, void* elem);
+void vector_pop_back(Vector* v);
+
+// Get/set element
+void* vector_get(Vector* v, size_t index);
+void vector_set(Vector* v, size_t index, void* elem);
 
 #endif
 
--- main.c
+++ main.c
@@ -2,13 +2,9 @@
 #include "include/vector.h"
 
 int main() {
-    Vector *v = vector_create(sizeof(int), 2);
-    if (!v) {
-        printf("Failed to create vector\n");
-        return 1;
-    }
-
+    Vector* v = vector_create(sizeof(int), 2);
     int input;
+
     printf("Enter integers (non-number to stop):\n");
     while (scanf("%d", &input) == 1) {
         vector_push_back(v, &input);
@@ -17,6 +13,14 @@ int main() {
     printf("\nYou entered %zu integers.\n", vector_size(v));
     printf("Vector capacity: %zu\n", vector_capacity(v));
     printf("Elements: ");
+    for (size_t i = 0; i < vector_size(v); i++) {
+        printf("%d ", *(int*)vector_get(v, i));
+    }
+    printf("\n");
+
+    // Example: remove last element
+    vector_pop_back(v);
+    printf("\nAfter pop_back, size: %zu, elements: ", vector_size(v));
     for (size_t i = 0; i < vector_size(v); i++) {
         printf("%d ", *(int*)vector_get(v, i));
     }
